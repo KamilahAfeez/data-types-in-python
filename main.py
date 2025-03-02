89f416a997460162f26096b86835daa3e373b15b
KamilahAfeez <kamilahafeez1@gmail.com>
2025-03-02 14:49:30 +0000

    data types inpython

---
+++ main.py
@@ -0,0 +1,11 @@
+age=12
+Name="Kamilah"
+Weight=48.6
+Student=True
+print("The datatype of age is: ",type(age))
+print("The datatype of name is: ",type(Name))
+print("The datatype of weight is: ",type(Weight))
+print("The datatype of student is: ",type(Student))
+print("*************************************************")
+print("The datat type of age is:",type(str(age)))
+print("The datat type of weight is:",type(int(Weight)))
